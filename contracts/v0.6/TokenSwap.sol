5e275d0898aa90f885cd679e9e3638b1f3220f94
Alex Roan <alex.roan@hotmail.com>
2021-01-24 19:04:19 +0000

    Token generic library

--- contracts/v0.6/TokenSwap.sol
+++ contracts/v0.6/TokenSwap.sol
@@ -13,7 +13,7 @@ library TokenSwap {
         uint256 toAmount,
         uint256 deadline
     ) internal {
-        address[] memory path = getTokenToLinkPath(from, to);
+        address[] memory path = getTokensPath(from, to);
         IERC20 erc20 = IERC20(from);
         uint256 tokenAmountIn = uniswapRouter.getAmountsIn(toAmount, path)[0];
         uint256 tokenBalance = erc20.balanceOf(address(this));
@@ -29,10 +29,10 @@ library TokenSwap {
             address(this),
             deadline
         );
-        require(amounts[1] >= toAmount, "Not enough LINK from swap");
+        require(amounts[1] >= toAmount, "Not enough output from swap");
     }
 
-    function getTokenToLinkPath(address from, address to) private pure returns (address[] memory) {
+    function getTokensPath(address from, address to) private pure returns (address[] memory) {
         address[] memory path = new address[](2);
         path[0] = from;
         path[1] = to;
